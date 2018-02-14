99ff52da3fbf6788678e40fb9a87940d47a08f1b
Paul Sherman <paul.sherman.88@gmail.com>
2018-02-14 01:17:13 -0600

    Pop back swiper when released

--- src/screens/Swiper.js
+++ src/screens/Swiper.js
@@ -12,22 +12,45 @@ type Props = {
   children: any
 };
 
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
+
 class Swiper extends React.Component<Props> {
   constructor(props) {
     super(props);
 
-    this.pan = new Animated.ValueXY();
+    this.pan = new Animated.Value(0);
+    this.scale = new Animated.Value(1);
+
     this.responder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event([
-        null,
-        { dx: this.pan.x, dy: this.pan.y }
-      ]),
-      onPanResponderRelease: () => {
-        Animated.spring(this.pan, {
-          toValue: { x: 0, y: 0},
-          friction: 4.5
-        }).start()
+      onPanResponderStart: () => {
+        Animated.timing(this.scale, {
+          toValue: 0.75
+        }).start();
+      },
+      onPanResponderMove: (event, gesture) => {
+        this.pan.setValue(gesture.dx);
+      },
+      onPanResponderRelease: (e, gesture) => {
+        const { vx } = gesture;
+        Animated.sequence([
+          // stop
+          Animated.decay(this.pan, {
+            velocity: vx,
+            deceleration: 0.99
+          }),
+          Animated.parallel([
+            // move back
+            Animated.timing(this.pan, {
+              toValue: 0,
+              friction: 10
+            }),
+            // scale back
+            Animated.timing(this.scale, {
+              toValue: 1
+            })
+          ]),
+        ]).start();
       }
     })
   }
@@ -35,18 +58,21 @@ class Swiper extends React.Component<Props> {
   render() {
     const animatedStyle = {
       transform: [
-        { translateX: this.pan.x },
-        { translateY: this.pan.y },
+        { translateX: this.pan },
+        { scale: this.scale }
       ]
     };
 
     return (
       <Animated.View
-        style={[styles.container, animatedStyle]}
         {...this.responder.panHandlers}
+        style={[styles.container]}
       >
-      
-        {this.props.children}
+        <Animated.View
+          style={[styles.container, animatedStyle]}
+        >
+          {this.props.children}
+        </Animated.View>
       </Animated.View>
     );
   }
