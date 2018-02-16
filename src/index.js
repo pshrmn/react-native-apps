5dabb248f02ca132dbbe5f765d70d0fb3b6beb7e
Paul Sherman <paul.sherman.88@gmail.com>
2018-02-16 01:01:45 -0600

    Move <Swiper> code to root component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Platform,
-  StyleSheet,
+  View,
   Text,
-  View
+  StyleSheet,
+  PanResponder,
+  Animated,
+  Dimensions
 } from 'react-native';
 import color from 'color';
 
 import ColorScreen from './screens/Color';
-import Swiper from './screens/Swiper';
 import randomColor from './utility/randomColor';
 
 type Props = {};
-class App extends Component<Props> {
+
+const TRIGGER_POINT = 0.15;
+
+class Swiper extends React.Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.pan = new Animated.Value(0);
+    this.opacity = new Animated.Value(1);
+
+    this.responder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        this.pan.setValue(gesture.dx);
+      },
+      onPanResponderStart: () => {
+        Animated.timing(this.opacity, {
+          toValue: 0
+        }).start();
+      },
+      onPanResponderRelease: (e, gesture) => {
+        const { dx, vx } = gesture;
+        const { width } = Dimensions.get('window');
+
+        const distance = Math.abs(dx);
+        const direction = dx / distance;
+
+        const velocity = Math.abs(vx);
+        const velocityDirection = vx / velocity;
+
+        const isLeaving = (distance / width > TRIGGER_POINT || Math.abs(vx) > 3) &&
+          !(velocityDirection !== direction && velocity > 0.5);
+        if (isLeaving) {
+          // animate off the screen, then swap colors
+          Animated.timing(this.pan, {
+            toValue: direction * width
+          }).start(() => {
+            this.handleSwipe();
+            this.pan.setValue(0);
+            this.opacity.setValue(1);
+          });
+        } else {
+          Animated.sequence([
+            // stop
+            Animated.decay(this.pan, {
+              velocity: vx,
+              deceleration: 0.99
+            }),
+            Animated.parallel([
+              // move back
+              Animated.timing(this.pan, {
+                toValue: 0,
+                friction: 10
+              }),
+              Animated.timing(this.opacity, {
+                toValue: 1
+              })
+            ])
+          ]).start();
+          
+        }
+      }
+    });  
+  }
 
   state = {
     foregroundColor: randomColor(),
@@ -43,22 +101,53 @@ class App extends Component<Props> {
   }
 
   render() {
+    const animatedStyle = {
+      transform: [
+        { translateX: this.pan }
+      ]
+    };
+    const opacityStyle = {
+      opacity: this.opacity
+    };
+
     const { foregroundColor, backgroundColor } = this.state;
-    const background = <ColorScreen color={backgroundColor} />;
 
     return (
-      <Swiper
-        onLeave={this.handleSwipe}
-        style={{ backgroundColor }}
+      <View
+        {...this.responder.panHandlers}
+        style={[
+          styles.container,
+          { backgroundColor }
+        ]}
       >
-        <ColorScreen color={foregroundColor} update={this.updateColor} />
-      </Swiper>
+        <Animated.View
+          style={[styles.foreground, animatedStyle]}
+        >
+          <ColorScreen
+            color={foregroundColor}
+            update={this.updateColor}
+            opacityStyle={opacityStyle}
+          />
+        </Animated.View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
-});
+  container: {
+    flex: 1,
+  },
+  foreground: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  background: {
+    flex: 1
+  }
+})
 
-export default App;
+export default Swiper;
